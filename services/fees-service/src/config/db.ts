8e7113ccf717fe050c351daf7e3902699b53d750
Nejmeddine <nejmeddinefekih01@gmail.com>
2025-05-19 12:01:46 +0100

    base

--- services/fees-service/src/config/db.ts
+++ services/fees-service/src/config/db.ts
@@ -3,17 +3,21 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-export const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT), 
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize('fees_db', 'root', 'myPassword', {
+  host: 'mysql-db-fees',
+  port: 3306,
+  dialect: 'mysql'
+});
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME!,
+//   process.env.DB_USER!,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT), 
+//     dialect: "mysql",
+//   }
+// );
 
 export const connectDB = async () => {
   try {
