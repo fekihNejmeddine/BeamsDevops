698ba12aad17bad6d7fca5861eb2795554711377
Nejmeddine <nejmeddinefekih01@gmail.com>
2025-05-19 13:55:54 +0100

    save express-http-proxy

--- services/gateway-service/src/utils/proxyRoutes.ts
+++ services/gateway-service/src/utils/proxyRoutes.ts
@@ -2,6 +2,6 @@ export const PROXY_ROUTES = [
   { path: "/auth", target: "http://auth_service:4001" },
   { path: "/api/notifications", target: "http://auth_service:4001" },
   { path: "/syndic", target: "http://syndic_service:4002" },
-  { path: "/fees", target: "http://192.168.2.124:4003" },
+  { path: "/fees", target: "http://fees_service:4003" },
   { path: "/meetings", target: "http://meeting_service:4004" },
 ];
