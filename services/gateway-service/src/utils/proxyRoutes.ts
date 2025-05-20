0acebba5fba27b6cb55f0ffd497458aa39d9c468
Nejmeddine <nejmeddinefekih01@gmail.com>
2025-05-20 14:08:28 +0100

    authentication

--- services/gateway-service/src/utils/proxyRoutes.ts
+++ services/gateway-service/src/utils/proxyRoutes.ts
@@ -1,5 +1,5 @@
 export const PROXY_ROUTES = [
-  { path: "/auth", target: "http://auth_service:4001" },
+  { path: "/auth", target: "http://192.168.2.124:4001" },
   { path: "/api/notifications", target: "http://auth_service:4001" },
   { path: "/syndic", target: "http://syndic_service:4002" },
   { path: "/fees", target: "http://192.168.2.124:4003" },
